be768e9269f993f9736aa14acad162d88b420f30
nathally11111 <nathally3004@gmail.com>
2025-08-11 17:34:34 -0300

    CEP

---
+++ cep.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consultar CEP</title>
+</head>
+<body>
+    <input name="cep" id="cep" type="text" />
+    <button onclick="">Consultar CEP</button>
+    <script>
+        var cep = document.getElementbyId("cep")
+    </script>
+</body>
+</html>
